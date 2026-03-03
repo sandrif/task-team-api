2acbd685bd97337ea90e4b085f7a39b5361cef0b
Sandra Faltysova <sandra.faltysova@sap.com>
2026-03-03 17:38:54 +0100

    chore: bootstrap Spring Boot project (web, actuator, jpa, flyway, postgresql)

---
+++ src/main/java/com/sandra/task_team_api/TaskTeamApiApplication.java
@@ -0,0 +1,13 @@
+package com.sandra.task_team_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTeamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTeamApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandra/task_team_api/TaskTeamApiApplicationTests.java
@@ -0,0 +1,21 @@
+package com.sandra.task_team_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = TaskTeamApiApplication.class)
+//		(properties = {
+//				"spring.autoconfigure.exclude=" +
+//						"org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
+//						"org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration," +
+//						"org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration"
+//		})
+
+
+class TaskTeamApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
